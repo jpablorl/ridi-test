8fd6c6ea44ab6cb24cf40cdb60a7f9f9133d70fb
Juan Pablo Rojas Lopez <rojas.tecnico.redes@gmail.com>
2020-08-31 16:04:49 -0500

    maqueta 16

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('.alert').hide();
+//$('.alert').hide();
 const btnRegInst = document.querySelector('#btnRegInst');
 btnRegInst.addEventListener('click',function(){
     window.location.assign('form_inst.html');
@@ -7,6 +7,7 @@ const btnRegAgr = document.querySelector('#btnRegAgr');
 btnRegAgr.addEventListener('click',function(){
     $('.alert').show();
 });
+
 $(".evento-item").click(function(event) {
     window.location.assign('evento.html');
 });
@@ -16,4 +17,20 @@ $("video").click(function(event) {
 });
 $(".visor").click(function(event) {
     $(".visor").fadeOut(400);
+});
+
+const btnBuscarCita = document.querySelector('#btnBuscarCita');
+btnBuscarCita.addEventListener('click',function(){
+    $('.alert').removeClass('alert-success');
+    $('.alert').addClass('alert-secondary');
+    $('.alert').show();
+});
+const btnCompartirCita = document.querySelector('#btnCompartirCita');
+btnCompartirCita.addEventListener('click',function(){
+    $('.alert').removeClass('alert-secondary');
+    $('.alert').addClass('alert-success');
+});
+const btnOcultarCita = document.querySelector('#btnOcultarCita');
+btnOcultarCita.addEventListener('click',function(){
+    $('.alert').hide();
 });
