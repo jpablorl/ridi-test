2e8952565e81dda98fd756e0ff8a9cbb01e5ae1a
Juan Pablo Rojas Lopez <rojas.tecnico.redes@gmail.com>
2020-09-01 12:17:43 -0500

    maqueta 19

--- js/script.js
+++ js/script.js
@@ -1,48 +1,64 @@
 //$('.alert').hide();
 const btnRegInst = document.querySelector('#btnRegInst');
-btnRegInst.addEventListener('click',function(){
+btnRegInst.addEventListener('click', function () {
     window.location.assign('form_inst.html');
 });
 const btnRegAgr = document.querySelector('#btnRegAgr');
-btnRegAgr.addEventListener('click',function(){
+btnRegAgr.addEventListener('click', function () {
     $('.alert').show();
 });
 
-$(".evento-item").click(function(event) {
+$(".evento-item").click(function (event) {
     window.location.assign('evento.html');
 });
 //Jquery
-$("video").click(function(event) {
+$("video").click(function (event) {
     $(".visor").fadeIn(400);
 });
-$(".visor").click(function(event) {
+$(".visor").click(function (event) {
     $(".visor").fadeOut(400);
 });
 const btnLogin = document.querySelector('#btnLogin');
-btnLogin.addEventListener('click',function(){
+btnLogin.addEventListener('click', function () {
     $('#exampleModalCenter5').modal('hide');
     $('#exampleModalCenter4').modal('show');
 });
 const btnBuscarCita = document.querySelector('#btnBuscarCita');
-btnBuscarCita.addEventListener('click',function(){
+btnBuscarCita.addEventListener('click', function () {
     $('.alert').removeClass('alert-success');
     $('.alert').addClass('alert-secondary');
     $('.alert').show();
 });
 const btnCompartirCita = document.querySelector('#btnCompartirCita');
-btnCompartirCita.addEventListener('click',function(){
+btnCompartirCita.addEventListener('click', function () {
     $('.alert').removeClass('alert-secondary');
     $('.alert').addClass('alert-success');
 });
 const btnOcultarCita = document.querySelector('#btnOcultarCita');
-btnOcultarCita.addEventListener('click',function(){
+btnOcultarCita.addEventListener('click', function () {
     $('.alert').hide();
 });
 //chat
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
+}
+
+function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
+}
+
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    //alert("Copied the text: " + copyText.value);
+}
